60407e2311f6c3bf0803af7727b5875e60700258
Ronak Jayesh Shukla <rjshukla21@gmail.com>
2022-03-30 12:42:36 -0700

    Adding header navigation to recipes and shopping-list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,9 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component} from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
 })
 export class HeaderComponent{
-    recipesSelected: boolean = true;
-    @Output() recipesSelectedEvent = new EventEmitter<boolean>();
-
-    onRecipesSelected() {
-        this.recipesSelected = true;
-        this.recipesSelectedEvent.emit(this.recipesSelected);
-    }
-
-    onShoppingListSelected() {
-        this.recipesSelected = false;
-        this.recipesSelectedEvent.emit(this.recipesSelected);
-    }
+    
 }
